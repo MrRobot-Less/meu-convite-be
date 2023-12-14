263c47bd9fed9d439eb8446f3a49c96b65cbec97
MrRobot-Less <gustavodev.dev@gmail.com>
2023-12-14 14:44:22 +0000

    fix: fixing middleware error

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -3,10 +3,11 @@ import { AppError } from "../dtos/error";
 
 export default function errorHandle(err: Error | AppError, req: Request, res: Response, next: NextFunction) {
 	var error: AppError;
-
-	if (err instanceof Error) error = new AppError(err.message, 500);
-	else error = err;
-
-	if (err) return res.status(error.status).json({ error: err.message });
+	
+	if (err) {
+		if (err instanceof Error) error = new AppError(err.message, 500);
+		else error = err;
+		return res.status(error.status).json({ error: err.message });
+	}
 	next();
 }
