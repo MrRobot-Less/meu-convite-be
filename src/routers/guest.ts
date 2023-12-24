ad58213d1e2f9c09f9b8380b45fb697fe522ca7a
MrRobot-Less <gustavodev.dev@gmail.com>
2023-12-24 00:48:39 +0000

    feat: adding the webhook about subscription on events route

--- src/routers/guest.ts
+++ src/routers/guest.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth";
 import GuestCtrl from "../controllers/guest";
+import subscriptionMiddleware from "../middlewares/subscription";
 
 const router = Router();
 
 // middleware to authenticate this router
 router.use(authMiddleware);
+router.use(subscriptionMiddleware);
 
 const controller = new GuestCtrl();
 router.delete(
