2f3761ba38eccfeb8dc149b12aadcf15fad6b5be
MrRobot-Less <gustavodev.dev@gmail.com>
2024-01-02 18:52:06 +0000

    feat!: fixing the no-cors error on frontend client.

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import errorHandle from './middlewares/error';
 import router from './routers';
+import cors from 'cors';
 
 import * as dotenv from "dotenv";
 dotenv.config({ path: process.env.PWD+'/.env' });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const corsOptions = {
+	origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }));
